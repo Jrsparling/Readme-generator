4c265acc17fb90ba4ac073ab83b7d3d758fdc913
Ziroll <jsparlingzi3757@gmail.com>
2024-05-31 00:29:27 -0500

    Added Javascript for index.js and generateMarkdown

--- index.js
+++ index.js
@@ -1,14 +1,78 @@
-// TODO: Include packages needed for this application
+        const inquirer = require("inquirer");
+        const generateMarkdown = require('./utils/generateMarkdown.js');
+        const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your project title?',
+    'Describe your project.',
+    'Check what table of contents sections apply.',
+    'Type in the steps to install your project.',
+    'Type how you use your project.',
+    'Select a license.',
+    'Enter contribution guidelines.',
+    'Type in testing parameters.',
+    'Type in your GitHub username.',
+    'Type in your email.'
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
+function init() { inquirer.prompt([
+                {
+            type: 'input',
+            message: questions[0],
+            name: 'Title', },
+                {
+            type: 'input',
+            message: questions[1],
+            name: 'Description', },
+                {
+            type: 'checkbox',
+            message: questions[2],
+            name: 'TableOfContents',
+            choices: [
+                "Description", 
+                "Installation",
+                "Usage", 
+                "License",
+                "Credits", 
+                "Contributing", 
+                "Tests", 
+                "Questions",
+            ] },
+                {
+            type: 'input',
+            message: questions[3],
+            name: 'Installation', },   
+                {
+            type: 'input',
+            message: questions[4],
+            name: 'Usage', },   
+                {
+            type: 'list',
+            message: questions[5],
+            name: 'License', },   
+                {
+            type: 'input',
+            message: questions[6],
+            name: 'Contributing', },   
+                {
+            type: 'input',
+            message: questions[7],
+            name: 'Tests', },
+                {
+            type: 'input',
+            message: questions[8],
+            name: 'GitHub' },
+            {
+            type: 'input',
+            message: questions[9],
+            name: 'Email',
+            },
+        ]) 
+        .then((data) => {
+            const README = generateMarkdown.generateMarkdownFn(data);
+            const fileName = `${data.title.toLowerCase().split(' ').join('_')}.md`;
+    });
+         fs.writeFile("filename", README, (err) => err ? console.log(err) : console.log("success! README has been created as", filename));
+    }
 // Function call to initialize app
 init();
-// appachie, mit, gnuv2, gnuv3, 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,98 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(License) {
+  if (License === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+}
+  else if (License === 'GPL v2') {
+    return '[![License: GNUv2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)'
+}
+else if (License === 'GPL v3')
+  return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+else if (License === 'Apache-2.0')
+  return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+else ('');
 }
 
+function renderLicenseLink(License) {
+  if (License === 'Mit') {
+    return '[Link](https://opensource.org/licenses/MIT)';
+  } else if (License === 'GPL v2') {
+    return '[Link](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+  } else if (License === 'GPL v3') {
+    return '[Link](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (License === 'Apache-2.0')
+    return '[Link](https://opensource.org/licenses/Apache-2.0)';
+    else ('')
+}
+
+function renderLicenseSection(License) {
+  if (License === 'Mit') {
+    return `##License
+    This project is covered under the MIT license.`;
+  } else if (License === 'GPL v2') {
+    return `##License
+    This project is covered under the GNU General Public License v2.0.`;
+}  else if (License === 'GPL v3') {
+  return `##License
+  This project is covered under the GNU General Public License v3.0.`;
+} else if (License === 'Apache-2.0') {
+  return `##License
+  This project is covered under the Apache License 2.0.`;
+} else ('')
+}
+
+function generateMarkdown(data){
+  
+  return `
+# ${data.title}
+
+## Description
+
+${data.Description}
+
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+
+[Installation](#Installation)
+<br/>
+[Usage](#Usage
+<br/>
+[Credits](#Credits)
+<br/>
+[License](#License)
+<br/>
+[How to Contribute](#Contributing)
+<br/>
+[Test](#Tests)
+<br/>
+[Questions](#Questions)
+<br/>
+
+## Installation
+
+${data.Installation}
+
+## Usage
+
+${data.Usage}
+
+## Credits
+
+${renderLicenseSection(data.License)}
+License ${renderLicenseLink(data.License)}
+
+## How to Contribute
+
+${data.Contributing}
+
+## Tests
+
+${data.Tests}
+
+## Questions
+My [GitHub](https://github.com/${data.GitHub}) profile
+
+Got questions? Reach out to me via email: [${data.email}](mailto:${data.email})}`;
+}
 module.exports = generateMarkdown;
